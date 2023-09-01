f709081d37d4dd5b677eea4f23060fc773b5d7a3
Yji <105905408+Testerbens@users.noreply.github.com>
2023-09-01 20:57:52 +0700

    Update index.js

--- index.js
+++ index.js
@@ -14,6 +14,20 @@ const yes = 'yes'
 return yes
 }
 
+function set(key, value, file) {
+   if(!key) return "Invalid Key! Use: create('key', 'value', './data.json')"
+   if(!value) return "Invalid Value! Use: create('key', 'value', './data.json')"
+   if(!file) return "enter the file name! Use: create('key', 'value', './data.json')"
+if(!fs.existsSync(file)) {
+   fs.writeFileSync(file, "{}"
+  }
+const json = JSON.parse(fs.readFileSync(file).toString())
+json[key] = value;
+fs.writeFileSync(file, JSON.stringify(json))
+const yes = 'yes'
+return yes
+}
+
 function get(key, file) {
    if(!key) return "enter key string! Use: get('key', './data.json')"
    if(!file) return "enter file path! Use: get('key', './data.json')"
@@ -22,6 +36,7 @@ return jason[key]
 }
 
 module.exports = {
-   get,
-   create
+   create,
+   set,
+   get
 }
