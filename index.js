14b25585a1d248591cef966e5609fea1598a37a2
Yji <105905408+Testerbens@users.noreply.github.com>
2023-09-01 21:19:06 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const fs = require('fs')
 
 function create(key, value, file) {
-   if(!key) return "Invalid Key! Use: create('key', 'value', './data.json')"
-   if(!value) return "Invalid Value! Use: create('key', 'value', './data.json')"
-   if(!file) return "enter the file name! Use: create('key', 'value', './data.json')"
+   if(!key) return new Error("Invalid Key! Use: create('key', 'value', './data.json')")
+   if(!value) return new Error("Invalid Value! Use: create('key', 'value', './data.json')")
+   if(!file) return new Error("enter the file name! Use: create('key', 'value', './data.json')")
 if(!fs.existsSync(file)) {
    fs.writeFileSync(file, "{}"
   }
@@ -15,9 +15,9 @@ return yes
 }
 
 function set(key, value, file) {
-   if(!key) return "Invalid Key! Use: create('key', 'value', './data.json')"
-   if(!value) return "Invalid Value! Use: create('key', 'value', './data.json')"
-   if(!file) return "enter the file name! Use: create('key', 'value', './data.json')"
+   if(!key) return new Error("Invalid Key! Use: create('key', 'value', './data.json')")
+   if(!value) return new Error("Invalid Value! Use: create('key', 'value', './data.json')")
+   if(!file) return new Error("enter the file name! Use: create('key', 'value', './data.json')")
 if(!fs.existsSync(file)) {
    fs.writeFileSync(file, "{}"
   }
@@ -29,14 +29,35 @@ return yes
 }
 
 function get(key, file) {
-   if(!key) return "enter key string! Use: get('key', './data.json')"
-   if(!file) return "enter file path! Use: get('key', './data.json')"
+   if(!key) return new Error("enter key string! Use: get('key', './data.json')")
+   if(!file) return new Error( "enter file path! Use: get('key', './data.json')")
  const jason = JSON.parse(fs.readFileSync(file).toString())
 return jason[key]
 }
+function delete(name, file) {
+   if(!name) return new Error("enter key name! Use: delete('key', 'data.json')")
+   if(!file) return new Error("enter file path! Use: delete('key', './data.json')")
+   let jason = JSON.parse(fs.readFileSync(file, "utf8"));
+delete jason[name];
+fs.writeFileSync(file, JSON.stringify(jason));
+return "";
+  },
+  function all(file) {
+     if(!file) return new Error("enter file path! Use: all('./data.json')")
+return JSON.parse(fs.readFileSync(file, "utf8"));
+  },
 
+  function has(name, file) {
+     if(!name) return new Error("enter key name! Use: has('key', './data')")
+     if(!file) return new Error("enter file path! Use: has('key', './data.json')")
+    let content = JSON.parse(fs.readFileSync(file, "utf8"));
+    return content[name] ? true : false;
+                 }
 module.exports = {
    create,
    set,
-   get
+   get,
+   delete,
+   all,
+   has
 }
