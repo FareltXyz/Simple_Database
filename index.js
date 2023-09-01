cfec1ae2d06a5477cf9d79c1863de9672d17e4dc
Yji <105905408+Testerbens@users.noreply.github.com>
2023-09-01 20:43:59 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const fs = require('fs')
 
 function create(key, value, file) {
+   if(!key) return "Invalid Key! Use: create('key', 'value', './data.json')"
+   if(!value) return "Invalid Value! Use: create('key', 'value', './data.json')"
+   if(!file) return "enter the file name! Use: create('key', 'value', './data.json')"
 if(!fs.existsSync(file)) {
    fs.writeFileSync(file, "{}"
   }
@@ -12,6 +15,8 @@ return yes
 }
 
 function get(key, file) {
+   if(!key) return "enter key string! Use: get('key', './data.json')"
+   if(!file) return "enter file path! Use: get('key', './data.json')"
  const jason = JSON.parse(fs.readFileSync(file).toString())
 return jason[key]
 }
